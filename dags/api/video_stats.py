c13ea3c857e476154246361a6644f472e7e0d7db
Optimus-Q <pereira.melroy@outlook.com>
2025-10-04 17:16:53 +0930

    Adding airflow updated files, dockers

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -2,15 +2,20 @@ import requests
 import json
 from datetime import date
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-load_dotenv(dotenv_path=".env")
+from airflow.decorators import task
+from airflow.models import Variable
 
-API_KEY = os.getenv("YOUTUBE_API_KEY")
+#load_dotenv(dotenv_path=".env")
+
+API_KEY = Variable.get("API_KEY")
+print(API_KEY)
 CHANNEL_HANDLE = "MrBeast"
 maxResults = 50
 
 
+@task
 def get_playlist_id():
 
     try:
@@ -36,7 +41,7 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         raise e
     
-
+@task
 def get_video_ids(playlistId):
 
     video_ids = []
@@ -74,7 +79,7 @@ def get_video_ids(playlistId):
     except requests.exceptions.RequestException as e:
         raise e
 
-
+@task
 def extract_video_data(video_ids):
 
     extracted_data = []
@@ -118,7 +123,7 @@ def extract_video_data(video_ids):
     except requests.exceptions.RequestException as e:
         raise e
 
-
+@task
 def save_to_json(extracted_data):
     file_path = f"./data/YT_data_{date.today()}.json"
 
